507047cdca24be7c7e43e2ecd46889fb4b5ae921
notrexxx <notrexx15@gmail.com>
2021-02-10 16:20:24 -0400

    Fix linter errors witn the percent string array

--- bubblesortby.rb
+++ bubblesortby.rb
@@ -17,6 +17,6 @@ def bubble_sort_by(str)
   print str
 end
 
-bubble_sort_by(%w['hi', 'hello', 'hey', 'idkwgh']) do |left, right|
+bubble_sort_by(%w[hi hello hey idkwgh]) do |left, right|
   left.length - right.length
 end
