ad16b88309dfad10fef6c00fd5a757a4ec4ac35b
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-02-10 18:06:33 -0400

    Fixed bubble_sort function by adding a do/while loop for multiple 'passings'

--- bubblesort.rb
+++ bubblesort.rb
@@ -2,25 +2,37 @@ def bubble_sort(arr)
   arr_length = arr.length
   last_element_i = arr_length - 1
 
-  arr.each_with_index do |_, i|
-    left_i = i
-    right_i = i + 1
+  loop do
+    swapped = false
 
-    right_i = last_element_i if right_i > last_element_i
+    arr.each_with_index do |_, i|
+      left_i = i
+      right_i = i + 1
 
-    left = arr[left_i]
-    right = arr[right_i]
+      if right_i > last_element_i
+        break
+      end
 
-    if left > right
-      arr[left_i] = right
-      arr[right_i] = left
+      left = arr[left_i]
+      right = arr[right_i]
+
+      if left > right
+        arr[left_i] = right
+        arr[right_i] = left
+        swapped = true
+      end
+    end
+
+    if !swapped
+      break
     end
   end
 
   arr
 end
 
-print bubble_sort([5, 3, 1, 6, 7, 2, 4, 8])
+unsorted = (1..10).to_a.reverse!
+print bubble_sort(unsorted)
 print "\n"
 
 def bubble_sort_by(str)
